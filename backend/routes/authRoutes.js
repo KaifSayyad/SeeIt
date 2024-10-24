fcdd9e7708afa92905c5f2f7d8bea53e90cc494c
KaifSayyad <kaifalisayyad@gmail.com>
2024-10-24 16:39:25 +0530

    Delete dist-1.1.0

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,9 @@ import { authenticateUser, addUser } from '../controllers/authController.js';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.send('Hello World from /auth');
+});
 router.post('/login', authenticateUser);
 router.post('/signup', addUser);
 
